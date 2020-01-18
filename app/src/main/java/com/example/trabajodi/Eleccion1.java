9b41779c3c878e3b6b42dafad84f790e1c8c6a75
SsrJon <jonanderdecastro@gmail.com>
2020-01-18 16:37:32 +0100

    pantallas 1 y 2

--- app/src/main/java/com/example/trabajodi/Eleccion1.java
+++ app/src/main/java/com/example/trabajodi/Eleccion1.java
@@ -2,14 +2,48 @@ package com.example.trabajodi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Eleccion1 extends AppCompatActivity {
+
+    private TextView tvElectricidad, tvGas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eleccion1);
+
+        tvElectricidad=findViewById(R.id.textViewElectricidad);
+        tvGas=findViewById(R.id.textViewGas);
+
+
+
+        tvElectricidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla2 = new Intent(Eleccion1.this, Eleccion2.class);
+                String valor  = "electricidad";
+                pantalla2.putExtra("valor", valor );
+                startActivity(pantalla2);
+
+            }
+        });
+
+        tvGas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla2 = new Intent(Eleccion1.this, Eleccion2.class);
+                String valor  = "gas";
+                pantalla2.putExtra("valor", valor );
+                startActivity(pantalla2);
+
+            }
+        });
 
 
 
--- app/src/main/java/com/example/trabajodi/Eleccion2.java
+++ app/src/main/java/com/example/trabajodi/Eleccion2.java
@@ -2,13 +2,93 @@ package com.example.trabajodi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Eleccion2 extends AppCompatActivity {
+
+    private TextView tvAlta, tvBaja, tvTitular, tvPotencia;
+    private Button btnAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eleccion2);
+
+        if(getIntent().getStringExtra("valor").equals("electricidad")){
+            getSupportActionBar().setTitle((R.string.electricidad));
+        }
+        else if(getIntent().getStringExtra("valor").equals("gas")){
+            getSupportActionBar().setTitle((R.string.gas));
+        }
+
+        tvAlta=findViewById(R.id.textViewAlta);
+        tvBaja=findViewById(R.id.textViewBaja);
+        tvTitular=findViewById(R.id.textViewTitular);
+        tvPotencia=findViewById(R.id.textViewPotencia);
+        btnAtras=findViewById(R.id.buttonAtras);
+
+
+
+
+        tvAlta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla3 = new Intent(Eleccion2.this, Validar.class);
+                startActivity(pantalla3);
+
+            }
+        });
+
+        tvBaja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla3 = new Intent(Eleccion2.this, Validar.class);
+                startActivity(pantalla3);
+
+
+            }
+        });
+
+
+        tvTitular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla3 = new Intent(Eleccion2.this, Validar.class);
+                startActivity(pantalla3);
+
+
+            }
+        });
+
+
+        tvPotencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                Intent pantalla3 = new Intent(Eleccion2.this, Validar.class);
+                startActivity(pantalla3);
+            }
+        });
+
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pantalla1 = new Intent(Eleccion2.this, Eleccion1.class);
+                startActivity(pantalla1);
+
+            }
+        });
+
     }
 }
